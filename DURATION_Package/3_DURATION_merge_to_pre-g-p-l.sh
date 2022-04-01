7d6bc7383d489c45cae4eafee1f164bd587b530b
Borisklwong <44147120+Borisklwong@users.noreply.github.com>
2022-04-01 14:49:09 +1100

    added parameters for quick changes between batches

--- DURATION_Package/3_DURATION_merge_to_pre-g-p-l.sh
+++ DURATION_Package/3_DURATION_merge_to_pre-g-p-l.sh
@@ -2,17 +2,16 @@
 #GENERATING DURATION_merge_to_pre-g-p-l
 #align with 
 #illumina Homo_sapiens.GRCh37
-
+date=23MAR2022_MESO
 ref=/home/users/allstaff/wong.b/onj_DURATION/tools/ref_data/gpl_ref_data_hg37/refgenomes/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta
-
-bam_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/120x
+bam_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/${date}
 
 for folder in $bam_dir/* ; do
         samplenames=$(basename $folder)
 	samplename=${samplenames/_process}
-        scp_outdir=/home/users/allstaff/wong.b/onj_DURATION/scripts/BGI_DURATION/120x/${samplename}_script
-        scriptname=$scp_outdir/${samplename}_merge_to_pre-g-p-l.sh
-	out_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/120x/${samplename}_process
+	out_dir=${bam_dir}/${samplename}_process
+	scp_outdir=${out_dir}/job
+	scriptname=$scp_outdir/${samplename}_merge_to_pre-g-p-l.sh
         echo Generating $scriptname
         mkdir -p ${scp_outdir}
 	mkdir -p ${out_dir}
