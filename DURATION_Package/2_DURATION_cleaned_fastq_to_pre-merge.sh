c900c9f63e8e3a965f53ea1fc843c2961422a9aa
Borisklwong <44147120+Borisklwong@users.noreply.github.com>
2022-04-01 14:47:53 +1100

    added parameter for quick changes between batches

--- DURATION_Package/2_DURATION_cleaned_fastq_to_pre-merge.sh
+++ DURATION_Package/2_DURATION_cleaned_fastq_to_pre-merge.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #GENERATING DURATION_cleaned_fastq_to_pre-merge
 #align with illumina Homo_sapiens.GRCh37
-threads=8
+threads=26
+date=23MAR2022_MESO
 ref=/home/users/allstaff/wong.b/onj_DURATION/tools/ref_data/gpl_ref_data_hg37/refgenomes/Homo_sapiens.GRCh37.GATK.illumina/Homo_sapiens.GRCh37.GATK.illumina.fasta
-process_data_folder=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/120x
+process_data_folder=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/${date}
 
 for fq1 in $process_data_folder/*/*_1.fq.gz ; do
         samplenames=$(basename $(dirname $fq1))
 	samplename=${samplenames/_process/}
         filename=$(basename $fq1 .fq.gz)
         fq2=${fq1/_1.fq/_2.fq}
-        scp_outdir=/home/users/allstaff/wong.b/onj_DURATION/scripts/BGI_DURATION/120x/${samplename}_script
-        scriptname=$scp_outdir/${filename}_wgs_to_pre-merge.sh
-	out_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/120x/${samplename}_process
+	out_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/${date}/${samplename}_process
+	scp_outdir=${out_dir}/job
+	scriptname=$scp_outdir/${filename}_wgs_to_pre-merge.sh
         echo Generating $scriptname
         mkdir -p ${scp_outdir}
         mkdir -p ${out_dir}
@@ -22,7 +23,7 @@ for fq1 in $process_data_folder/*/*_1.fq.gz ; do
 #SBATCH --ntasks-per-node=$threads
 #SBATCH --job-name=DURATION_cleaned_fastq_to_pre-merge
 #SBATCH --time=48:00:00
-#SBATCH --mem=10G
+#SBATCH --mem=64G
 # THIS IS A GENERATED SCRIPT
 #DURATION_cleaned_fastq_to_pre-merge
 #
