16bff83e06faf0dd152d2654a91b72c8fd31ee1f
Borisklwong <44147120+Borisklwong@users.noreply.github.com>
2022-04-01 14:44:57 +1100

    added parameters for quick changes between batches
    parameters added:
    date
    adapter_sequence
    adapter_sequence_r2

--- DURATION_Package/1_DURATION_fastp_job.sh
+++ DURATION_Package/1_DURATION_fastp_job.sh
@@ -10,18 +10,20 @@
 #align with 
 #illumina Homo_sapiens.GRCh37
 
+date=23MAR2022_MESO
+adapter_sequence=AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA
+adapter_sequence_r2=AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG
+data_folder=/home/users/allstaff/wong.b/onj_DURATION/raw_data/BGI_samples/BGI_MESO
 
-data_folder=/home/users/allstaff/wong.b/onj_DURATION/raw_data/BGI_DURATION/FFPE_FF_BC_120x/Raw_Fastq/WGS_2021_016
-
-for fq1 in $data_folder/*/*_1.fq.gz ; do
+for fq1 in $data_folder/*/*/*_1.fq.gz ; do
         samplename=$(basename $(dirname $fq1))
         filename=$(basename $fq1 .fq.gz)
         fq2=${fq1/_1.fq/_2.fq}
 	fq1_clean=${filename}_clean_1.fq.gz
 	fq2_clean=${filename}_clean_2.fq.gz
-	out_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/120x/${samplename}_process
-	job_dir=${out_dir}/.job
-        job_file=${job_dir}/${sample_name}_fastp.sh
+	out_dir=/home/users/allstaff/wong.b/onj_DURATION/processing/BGI_DURATION_processing/${date}/${samplename}_process
+	job_dir=${out_dir}/job
+        job_file=${job_dir}/${samplename}_fastp.sh
 		echo Generating ${job_file}
         mkdir -p ${out_dir}
 	mkdir -p ${job_dir}
@@ -39,8 +41,8 @@ cd ${out_dir}
 ~/onj_DURATION/tools/fastp/fastp \
 -i ${fq1} \
 -I ${fq2} \
---adapter_sequence=AAGTCGGAGGCCAAGCGGTCTTAGGAAGACAA \
---adapter_sequence_r2=AAGTCGGATCGTAGCCATGTCGTTCTGTGAGCCAAGGAGTTG \
+--adapter_sequence=${adapter_sequence} \
+--adapter_sequence_r2=${adapter_sequence_r2} \
 -o ${fq1_clean} \
 -O ${fq2_clean} \
 -j ${filename}_fastp.json \
